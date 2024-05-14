58e4b0327c0278e2a1388ac37f2c1ec355dc30eb
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-05-14 10:13:36 +0100

    Add useful descriptors for experiment class

--- src/nxstacker/utils/model.py
+++ src/nxstacker/utils/model.py
@@ -1,4 +1,8 @@
 from datetime import datetime, timedelta, tzinfo
+from pathlib import Path
+
+from nxstacker.parser.proj_identifier import generate_numbers
+from nxstacker.utils.facility import choose_facility_info
 
 
 class UKtz(tzinfo):
@@ -52,3 +56,130 @@ class UKtz(tzinfo):
                     return last_sunday
 
             last_day -= 1
+
+
+class ReadOnly:
+    """A read-only descriptor."""
+
+    def __set_name__(self, owner, name):
+        self.public_name = name
+        self.private_name = f"_{name}"
+
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self
+        return getattr(instance, self.private_name)
+
+    def __set__(self, instance, value):
+        msg = f"can't set attribute '{self.public_name}'"
+        raise AttributeError(msg)
+
+
+class Directory(ReadOnly):
+    """Represent a directory."""
+
+    def __init__(self, *, undefined_ok=False, must_exist=False):
+        """Initialise the directory descriptor.
+
+        Parameters
+        ----------
+        undefined_ok : bool, optional
+            whether the directory can be None. Default to False.
+        must_exist : bool, optional
+            whether to check for the existence of directory. Default to
+            False, and it will be created if the path is invalid.
+
+        """
+        self.undefined_ok = undefined_ok
+        self.must_exist = must_exist
+
+    def __set__(self, instance, value):
+        if hasattr(instance, self.private_name):
+            msg = f"can't set attribute '{self.public_name}'"
+            raise AttributeError(msg)
+
+        if value is None and self.undefined_ok:
+            dir_ = None
+        elif value is None:
+            dir_ = Path()
+        else:
+            dir_ = Path(value)
+
+        self.validate_or_create(dir_)
+        setattr(instance, self.private_name, dir_)
+
+    def validate_or_create(self, dir_):
+        """Check directory existence and create it if it is necessary.
+
+        Parameters
+        ----------
+        dir_ : pathlib.Path or None
+            the direcotry, or None and validation is skipped.
+
+        """
+        # skip validation if it is None
+        if dir_ is None:
+            return
+
+        if self.must_exist and not dir_.is_dir():
+            msg = f"The directory {dir_} does not exist."
+            raise ValueError(msg)
+
+        if not dir_.is_dir():
+            dir_.mkdir(parents=True, exist_ok=True)
+
+
+class IdentifierRange(ReadOnly):
+    """Represent a range of projection identifiers."""
+
+    def __init__(self, num_type=int):
+        """Initialise the identifier's range descriptor.
+
+        Parameters
+        ----------
+        num_type : type,, optional
+            the data type of the identifier. Default to int. This is
+            used when the range is generated. They will be converted to
+            str at the end.
+
+        """
+        self.num_type = num_type
+
+    def __set__(self, instance, value):
+        if hasattr(instance, self.private_name):
+            msg = f"can't set attribute '{self.public_name}'"
+            raise AttributeError(msg)
+
+        if value is None:
+            id_rng = ()
+        elif isinstance(value, str):
+            id_rng = generate_numbers(value, dtype=self.num_type)
+        else:
+            id_rng = tuple(value)
+
+        id_rng_as_str = tuple(str(k) for k in id_rng)
+        setattr(instance, self.private_name, id_rng_as_str)
+
+
+class ExperimentFacility(ReadOnly):
+    """Represent a facility info in an experiment."""
+
+    def __set__(self, instance, value):
+        if hasattr(instance, self.private_name):
+            msg = f"can't set attribute '{self.public_name}'"
+            raise AttributeError(msg)
+
+        if value is None or isinstance(value, str):
+            # these are all directories which can help deducing facility
+            # in case they are not defined
+            associated_dir = [
+                instance.__dict__.get("_proj_dir"),
+                instance.__dict__.get("_nxtomo_dir"),
+                instance.__dict__.get("_raw_dir"),
+            ]
+
+            facility = choose_facility_info(value, dirs=associated_dir)
+        else:
+            facility = value
+
+        setattr(instance, self.private_name, facility)
