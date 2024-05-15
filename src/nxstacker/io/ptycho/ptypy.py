3ddf3253f9ee759173398d770cb72393f02dfd97
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-05-15 11:40:14 +0100

    Use descriptors in ptypy file

--- src/nxstacker/io/ptycho/ptypy.py
+++ src/nxstacker/io/ptycho/ptypy.py
@@ -6,64 +6,141 @@ import h5py
 import numpy as np
 
 from nxstacker.utils.io import file_has_paths, top_level_dir
+from nxstacker.utils.model import (
+    Directory,
+    FilePath,
+    FixedValue,
+    PositiveNumber,
+)
 from nxstacker.utils.parse import quote_iterable
 
 
 class PtyPyFile:
+    """Represent a PtyPy reconstruction file."""
 
     experiment = "ptychography"
     software = "PtyPy"
-    path_names = MappingProxyType({"object": "/content/obj",
-                                   "probe": "/content/probe",
-                                   "scan_names":  "/content/pars/scans",
-                                   })
+    path_names = MappingProxyType(
+        {
+            "object": "/content/obj",
+            "probe": "/content/probe",
+            "scan_names": "/content/pars/scans",
+        },
+    )
     essential_paths = tuple(path_names.values())
-    extensions = (".ptyr", )
+    extensions = (".ptyr",)
+    file_path = FilePath(must_exist=True)
+    scan_name = FixedValue()
+    storage_name = FixedValue()
+    object_path = FixedValue()
+    probe_path = FixedValue()
+    raw_file_path = FixedValue()
+    px_sz_path = FixedValue()
+    raw_file = FilePath(must_exist=True)
+    id_scan = FixedValue()
+    id_proj = FixedValue()
+    id_angle = FixedValue()
+    distance = PositiveNumber(float)
+    pixel_size = PositiveNumber(float)
+    description = FixedValue()
+    trim_proj = FixedValue()
+    raw_dir = Directory(undefined_ok=True, must_exist=True)
+    object_shape = FixedValue()
+    object_complex_dtype = FixedValue()
+    object_modulus_dtype = FixedValue()
+    object_phase_dtype = FixedValue()
+    probe_shape = FixedValue()
+    probe_complex_dtype = FixedValue()
+    probe_modulus_dtype = FixedValue()
+    probe_phase_dtype = FixedValue()
 
-    def __init__(self, file_path, id_scan=None, id_proj=None, *, verify=True):
-        self._file_path = Path(file_path)
+    def __init__(
+        self,
+        file_path,
+        id_scan=None,
+        id_proj=None,
+        *,
+        verify=True,
+        raw_dir=None,
+        description=None,
+    ):
+        """Initialise an instance of PtyPy file.
+
+        Parameters
+        ----------
+        file_path : pathlib.Path or str
+            the file path of the reconstruction file.
+        id_scan : str or None, optional
+            the scan identifier of the reconstruction. If it is None, it
+            tries to fetch this from the file and its attributes.
+            Default to None.
+        id_proj : str or None, optional
+            the projection identifier of the reconstruction. If it is
+            None, it tries to fetch this from the file and its
+            attributes. Default to None.
+        verify : bool, optional
+            whether to verify the reconstruction file contains a minimum
+            set of keys specific to the software from it is produced.
+            Default to True.
+        raw_dir : pathlib.Path or str, optional
+            the directory that stores the raw data of this
+            reconstruction.  Default to None, and it will be deduced
+            from the attributes of the reconstruction file.
+        description : str, optional
+            a meaningful description about the sample of the
+            reconstruction. Default to None.
+
+        """
+        self.file_path = file_path
 
         if verify and not self.verify_file():
             paths = quote_iterable(self.essential_paths)
-            msg = (f"The file {self._file_path} is not a reconstruction "
-                   f"from {self.software}. Does the file have all the "
-                   f"following path names: {paths}?")
+            msg = (
+                f"The file {self._file_path} is not a reconstruction "
+                f"from {self.software}. Does the file have all the "
+                f"following path names: {paths}?"
+            )
             raise KeyError(msg)
 
         self._compose_paths()
 
         if id_scan is None:
-            self._id_scan = self._retrieve_id_scan()
+            self.id_scan = self._retrieve_id_scan()
         else:
-            self._id_scan = str(id_scan)
+            self.id_scan = str(id_scan)
 
         if id_proj is None:
-            self._id_proj = self._retrieve_id_proj()
+            self.id_proj = self._retrieve_id_proj()
         else:
-            self._id_proj = str(id_proj)
+            self.id_proj = str(id_proj)
 
-        # these are not known at this stage
-        self._id_angle = None
-        self._distance = None
-        self._pixel_size = None
-        self.description = None
+        # the rotation angle is not known at this stage
+        # this is available from the raw data file and retrieval of it
+        # depends on the facility
+        self.id_angle = None
 
+        self.raw_dir = raw_dir
+        self.description = description
         self.trim_proj = True
 
     def fill_attr(self):
-        self.find_raw_dir()
+        """Assign attributes as determined from the file."""
+        if self.raw_dir is None:
+            self._overwrite_raw_dir()
+
         self._ob_attr()
         self._pr_attr()
 
     def _retrieve_id_scan(self):
         # get the scan number from the raw file by matching them with
         # some regex
-        regexes = (re.compile(r".*-(\d+)\.nxs$"),
-                   re.compile(r".*-(\d+)_processed\.nxs$"),
-                   )
+        regexes = (
+            re.compile(r".*-(\d+)\.nxs$"),
+            re.compile(r".*-(\d+)_processed\.nxs$"),
+        )
 
         for regex in regexes:
-            match = re.search(regex, self._raw_file)
+            match = re.search(regex, str(self._raw_file))
             if match is not None:
                 # return the first match
                 return match.group(1)
@@ -71,12 +148,13 @@ class PtyPyFile:
         msg = f"Cannot deduce scan numbers from {self._raw_file}"
         raise RuntimeError(msg)
 
-
     def _retrieve_id_proj(self):
-        msg = (f"For {self.software} reconstruction file, it cannot get the "
-                "projection ID from the file itself. It should be set by "
-                "passing value to the argument 'id_proj' when initialising "
-                "the instance.")
+        msg = (
+            f"For {self.software} reconstruction file, it cannot get the "
+            "projection ID from the file itself. It should be set by "
+            "passing value to the argument 'id_proj' when initialising "
+            "the instance."
+        )
         raise TypeError(msg)
 
     def _compose_paths(self):
@@ -84,207 +162,145 @@ class PtyPyFile:
             scans = list(f[self.path_names["scan_names"]])
 
             # assume one storage
-            scan_name = scans[0]
+            self.scan_name = scans[0]
 
-            self._storage = f"S{scan_name}G00"
-            self._object_path = (f"{self.path_names['object']}/"
-                                 f"{self._storage}/data")
-            self._probe_path = (f"{self.path_names['probe']}/"
-                                f"{self._storage}/data")
-            self._raw_file_key = (f"{self.path_names['scan_names']}/"
-                                  f"{scan_name}/data/intensities/file")
-            self._px_sz_key = (f"{self.path_names['object']}/"
-                               f"{self._storage}/_psize")
+            self.storage_name = f"S{self._scan_name}G00"
+            self.object_path = (
+                f"{self.path_names['object']}/{self._storage_name}/data"
+            )
+            self.probe_path = (
+                f"{self.path_names['probe']}/{self._storage_name}/data"
+            )
+            self.raw_file_path = (
+                f"{self.path_names['scan_names']}/"
+                f"{self._scan_name}/data/intensities/file"
+            )
+            self.px_sz_path = (
+                f"{self.path_names['object']}/{self._storage_name}/_psize"
+            )
 
             # get the path of the raw file
-            self._raw_file = f[self._raw_file_key][()]
+            self.raw_file = f[self._raw_file_path][()]
 
-        if isinstance(self._raw_file, bytes):
-            self._raw_file = self._raw_file.decode()
-
-
-    def find_raw_dir(self):
+    def _overwrite_raw_dir(self):
+        """Overwrite the _raw_dir attribute."""
         raw_dir = top_level_dir(self._raw_file)
 
         if Path(raw_dir).is_dir():
-            self._raw_dir =  raw_dir
+            self._raw_dir = raw_dir
         else:
-            self._raw_dir =  top_level_dir(self._file_path)
+            self._raw_dir = top_level_dir(self._file_path)
 
     def object_complex(self, mode=0):
+        """Return the complex object of a particular mode.
+
+        Parameters
+        ----------
+        mode : int, optional
+            the mode of the complex object to be returned. Default to 0.
+
+        """
         with h5py.File(self._file_path, "r") as f:
             ob = f[self._object_path]
 
             if mode < (num_modes := ob.shape[0]):
                 obj = ob[mode, :, :]
             else:
-                mode_str = "mode" + "s"*(num_modes>1)
-                msg = (f"The object has {num_modes} {mode_str} and the "
-                       f"maximum mode index is {num_modes-1}, but {mode} "
-                        "was given.")
+                mode_str = "mode" + "s" * (num_modes > 1)
+                msg = (
+                    f"The object has {num_modes} {mode_str} and the "
+                    f"maximum mode index is {num_modes-1}, but {mode} "
+                    "was given."
+                )
                 raise IndexError(msg)
         return obj
 
     def object_modulus(self, mode=0):
+        """Return the object modulus of a particular mode."""
         return np.abs(self.object_complex(mode=mode))
 
     def object_phase(self, mode=0):
+        """Return the object phase of a particular mode."""
         return np.angle(self.object_complex(mode=mode))
 
     def _ob_attr(self):
         with h5py.File(self._file_path, "r") as f:
-            self._object_shape = f[self._object_path].shape
-            self._object_complex_dtype = f[self._object_path].dtype
-            self._pixel_size = f[self._px_sz_key][()].mean()
+            self.object_shape = f[self._object_path].shape
+            self.object_complex_dtype = f[self._object_path].dtype
+            self.pixel_size = f[self._px_sz_path][()].mean()
 
         # don't want to perform np.abs and np.angle by reading the
         # actual complex data
-        if self._object_complex_dtype == np.complex64:
-            self._object_modulus_dtype = np.float32
-            self._object_phase_dtype = np.float32
+        if self.object_complex_dtype == np.complex64:
+            self.object_modulus_dtype = np.float32
+            self.object_phase_dtype = np.float32
         else:
-            self._object_modulus_dtype = np.float64
-            self._object_phase_dtype = np.float64
+            self.object_modulus_dtype = np.float64
+            self.object_phase_dtype = np.float64
 
     def probe_complex(self, mode=0):
+        """Return the complex probe of a particular mode.
+
+        Parameters
+        ----------
+        mode : int, optional
+            the mode of the complex probe to be returned. Default to 0.
+
+        """
         with h5py.File(self._file_path, "r") as f:
             pr = f[self._probe_path]
 
             if mode < (num_modes := pr.shape[0]):
                 prb = pr[mode, :, :]
             else:
-                mode_str = "mode" + "s"*(num_modes>1)
-                msg = (f"The probe has {num_modes} {mode_str} and the "
-                       f"maximum mode index is {num_modes-1}, but {mode} "
-                        "was given.")
+                mode_str = "mode" + "s" * (num_modes > 1)
+                msg = (
+                    f"The probe has {num_modes} {mode_str} and the "
+                    f"maximum mode index is {num_modes-1}, but {mode} "
+                    "was given."
+                )
                 raise IndexError(msg)
         return prb
 
     def probe_modulus(self, mode=0):
+        """Return the probe modulus of a particular mode."""
         return np.abs(self.probe_complex(mode=mode))
 
     def probe_phase(self, mode=0):
+        """Return the probe phase of a particular mode."""
         return np.angle(self.probe_complex(mode=mode))
 
     def _pr_attr(self):
         with h5py.File(self._file_path, "r") as f:
-            self._probe_shape = f[self._probe_path].shape
-            self._probe_complex_dtype = f[self._probe_path].dtype
+            self.probe_shape = f[self._probe_path].shape
+            self.probe_complex_dtype = f[self._probe_path].dtype
 
         # don't want to perform np.abs and np.angle by reading the
         # actual complex data
-        if self._probe_complex_dtype == np.complex64:
-            self._probe_modulus_dtype = np.float32
-            self._probe_phase_dtype = np.float32
+        if self.probe_complex_dtype == np.complex64:
+            self.probe_modulus_dtype = np.float32
+            self.probe_phase_dtype = np.float32
         else:
-            self._probe_modulus_dtype = np.float64
-            self._probe_phase_dtype = np.float64
+            self.probe_modulus_dtype = np.float64
+            self.probe_phase_dtype = np.float64
 
     def verify_file(self):
+        """Check existence of some essential hdf5 paths."""
         return file_has_paths(self._file_path, self.essential_paths)
 
     @property
-    def file_path(self):
-        return self._file_path
-
-    @property
-    def storage(self):
-        return self._storage
-
-    @property
-    def object_path(self):
-        return self._object_path
-
-    @property
-    def probe_path(self):
-        return self._probe_path
-
-    @property
-    def raw_file(self):
-        return self._raw_file
-
-    @property
-    def raw_dir(self):
-        return self._raw_dir
-
-    @property
-    def object_shape(self):
-        return self._object_shape
-
-    @property
-    def object_complex_dtype(self):
-        return self._object_complex_dtype
-
-    @property
-    def object_modulus_dtype(self):
-        return self._object_modulus_dtype
-
-    @property
-    def object_phase_dtype(self):
-        return self._object_phase_dtype
-
-    @property
-    def probe_shape(self):
-        return self._probe_shape
-
-    @property
-    def probe_complex_dtype(self):
-        return self._probe_complex_dtype
-
-    @property
-    def probe_modulus_dtype(self):
-        return self._probe_modulus_dtype
-
-    @property
-    def probe_phase_dtype(self):
-        return self._probe_phase_dtype
-
-    @property
-    def id_scan(self):
-        if self._id_scan is None:
-            self._id_scan = self._retrieve_id_scan()
-        return self._id_scan
-
-    @property
-    def id_proj(self):
-        if self._id_proj is None:
-            self._id_proj = self._retrieve_id_proj()
-        return self._id_proj
-
-    @property
-    def id_angle(self):
-        return self._id_angle
-
-    @id_angle.setter
-    def id_angle(self, angle):
-        self._id_angle = angle
-
-    @property
-    def distance(self):
-        return self._distance
-
-    @distance.setter
-    def distance(self, dist):
-        self._distance = dist
-
-    @property
-    def pixel_size(self):
-        return self._pixel_size
-
-    @pixel_size.setter
-    def pixel_size(self, px_sz):
-        self._pixel_size = px_sz
-
-    @property
     def avail_complex(self):
+        """Indicate whether the complex object/probe is available."""
         return True
 
     @property
     def avail_modulus(self):
+        """Indicate whether the modulus of object/probe is available."""
         return True
 
     @property
     def avail_phase(self):
+        """Indicate whether the phase of object/probe is available."""
         return True
 
     def __str__(self):
@@ -292,9 +308,11 @@ class PtyPyFile:
 
     def __repr__(self):
         cls_name = type(self).__name__
-        id_angle = (f"id_angle={self._id_angle}" if self._id_angle is not None
-                    else "")
-        return (f"{cls_name}(file_path='{self._file_path}',"
-                f"id_scan='{self._id_scan}',"
-                f"id_proj='{self._id_proj}'{id_angle})")
-
+        id_angle = (
+            f"id_angle={self._id_angle}" if self._id_angle is not None else ""
+        )
+        return (
+            f"{cls_name}(file_path='{self._file_path}',"
+            f"id_scan='{self._id_scan}',"
+            f"id_proj='{self._id_proj}'{id_angle})"
+        )
