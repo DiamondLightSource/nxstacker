134bd22afb4bb8d270d3adc38c4acd004ff2deac
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-05-15 17:05:43 +0100

    Document the projection identifier class

--- src/nxstacker/parser/proj_identifier.py
+++ src/nxstacker/parser/proj_identifier.py
@@ -6,7 +6,18 @@ import numpy as np
 
 
 def generate_numbers(specifier, dtype=int):
-    """
+    """Generate a list of numbers from specifier.
+
+    The specifier should have the format of <START>[-<END>[:<STEP>]], for
+    examples, "10-12" gives [10, 11, 12], "10-14:2" gives [10, 12, 14].
+
+    Parameters
+    ----------
+    specifier : str
+        the specifier for the generation of number
+    dtype : type, optional
+        the data type of the numbers. Default to int.
+
     """
     result = []
     segments = specifier.split(",")
@@ -18,50 +29,66 @@ def generate_numbers(specifier, dtype=int):
 
     single_num = re.compile(r"^([-]?\d+\.?\d*)$")
     start_end = re.compile(r"^([-]?\d+\.?\d*)-([-]?\d+\.?\d*)$")
-    start_end_step = re.compile(r"^([-]?\d+\.?\d*)-([-]?\d+\.?\d*):"
-                                r"(\d+\.?\d*)$")
-
+    start_end_step = re.compile(
+        r"^([-]?\d+\.?\d*)-([-]?\d+\.?\d*):" r"(\d+\.?\d*)$"
+    )
 
     for segment in segments:
-        if (match := re.search(single_num, segment)):
+        if match := re.search(single_num, segment):
             start = float(match.group(1))
             end = start
             step = 1.0
-        elif (match := re.search(start_end, segment)):
+        elif match := re.search(start_end, segment):
             start = float(match.group(1))
             end = float(match.group(2))
             step = 1.0
-        elif (match := re.search(start_end_step, segment)):
+        elif match := re.search(start_end_step, segment):
             start = float(match.group(1))
             end = float(match.group(2))
             step = float(match.group(3))
         else:
-            msg = (f"Invalid specifier {segment}. It should be one of the "
-                    "followings: <NUMBER>, <START>-<END> or "
-                    "<START>-<END>:<STEP>.")
+            msg = (
+                f"Invalid specifier {segment}. It should be one of the "
+                "followings: <NUMBER>, <START>-<END> or "
+                "<START>-<END>:<STEP>."
+            )
             raise ValueError(msg)
 
-        result.extend(list(np.arange(start, end+eps, step).astype(dtype)))
+        result.extend(list(np.arange(start, end + eps, step).astype(dtype)))
 
     return result
 
 
 class ProjIdentifier:
-    """Specify the identifiers for projections.
+    """Specify the identifiers for projections."""
 
-    Common identifiers for projections are by scan numbers, projection
-    numbers, and rotation angles.
+    def __init__(
+        self, from_range=None, from_file=None, exclude=None, id_type=str
+    ):
+        """Initialise the projection identifiers.
 
-    Attributes
-    ----------
-    from_range : list
-    from_file : list
-    exclude : list
-    """
+        The source can be from a range specified as
+        <START>[-<END>[:<STEP>]] and/or a text file. If there is any
+        duplicate, it will be removed.
 
-    def __init__(self, from_range=None, from_file=None, exclude=None,
-                 id_type=str):
+        If the id_type is float-point number, it may suffer from
+        precision issue.
 
+        Parameters
+        ----------
+        from_range : str, optional
+            the range specified as <START>[-<END>[:<STEP>]]. Default to
+            None.
+        from_file : str or pathlib.Path, optional
+            the text file with the identifier in a single column.
+            Default to None.
+        exclude : str, optional
+            identifier to be exclude, specified as
+            <START>[-<END>[:<STEP>]].
+        id_type : type, optional
+            the data type of the identifier. Default to str.
+
+        """
         self.from_range = []
         self.from_file = []
         self.exclude = []
@@ -78,8 +105,9 @@ class ProjIdentifier:
             self.exclude = generate_numbers(exclude, self.id_type)
 
         # retain order, a bit inefficient
-        merged = list(dict.fromkeys(self.from_range)) +\
-                 list(dict.fromkeys(self.from_file))
+        merged = list(dict.fromkeys(self.from_range)) + list(
+            dict.fromkeys(self.from_file)
+        )
         merged = list(dict.fromkeys(merged))
         for entry in merged:
             if entry not in self.exclude:
@@ -88,12 +116,26 @@ class ProjIdentifier:
         self.identifiers = tuple(self.identifiers)
 
     def id_from_range(self, specifier):
+        """Return numbers from <START>[-<END>[:<STEP>]]."""
         return generate_numbers(specifier, self.id_type)
 
-    def id_from_file(self, file_path):
+    def id_from_file(self, file_path, delimiter=None):
+        """Return numbers from a text file.
+
+        Parameters
+        ----------
+        file_path : str or pathlib.Path
+            the text file with the numbers in a single column
+        delimiter : str, optional
+            the delimiter between columns if there are mutliple columns.
+            Default to None, set to space " ".
+
+        """
+        if delimiter is None:
+            delimiter = " "
+
         fp = Path(file_path)
         with fp.open() as f:
-            # only first column, assume delimited by space if more
-            # columns are present in the file
-            ids = [self.id_type(entry.split(" ")[0]) for entry in f]
+            # only first column
+            ids = [self.id_type(entry.split(delimiter)[0]) for entry in f]
         return ids
