9e048867cac7b7349f97962bf97bba0d9b01585d
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-05-16 09:57:35 +0100

    Update docs in tomojoin

--- src/nxstacker/tomojoin.py
+++ src/nxstacker/tomojoin.py
@@ -10,37 +10,86 @@ def tomojoin_entry():
     args = parse()
     tomojoin(**args)
 
-def tomojoin(experiment_type, facility=None, proj_dir=None, nxtomo_dir=None,
-             include_scan=None, include_proj=None, include_angle=None,
-             raw_dir=None, *, sort_by_angle=False, pad_to_max=True,
-             compress=False, **kwargs):
+
+def tomojoin(
+    experiment_type,
+    facility=None,
+    proj_dir=None,
+    nxtomo_dir=None,
+    include_scan=None,
+    include_proj=None,
+    include_angle=None,
+    raw_dir=None,
+    *,
+    sort_by_angle=False,
+    pad_to_max=True,
+    compress=False,
+    **kwargs,
+):
     """Combine projections to produce an NXtomo file.
 
     Parameters
     ----------
     experiment_type : str
+        the type of experiment
+    facility : FacilityInfo, str or None, optional
+        the facility. It could be of the class FacilityInfo, which
+        already contains the details, or a str, where an instance of
+        FacilityInfo is initialised, or None, where the
+        corresponding facility is deduced from given directories.
+        Default to None.
+    proj_dir : pathlib.Path, str or None
+        the directory where the projections are stored. If it is
+        None, the current working directory is used. Default to None.
+    nxtomo_dir : pathlib.Path, str or None
+        the directory where the NXtomo files will be saved. If it is
+        None, the current working directory is used. Default to None.
+    include_scan : str, iterable or None
+        the identifiers of scans to include in the NXtomo file. If
+        it is a str, it is passed to generate_numbers; if it is an
+        iterable, it will convert to a tuple; if it is None, it will
+        be an empty tuple. Default to None.
+    include_proj : str, iterable or None
+        the identifiers of projections to include in the NXtomo
+        file. See "include_scan". Default to None.
+    include_angle : str, iterable or None
+        the rotation angles to be included in the NXtomo file. Use
+        this with caution as it might suffer from float-point
+        precision issue.  See "include_scan". Default to None.
+    raw_dir : pathlib.Path, str or None, optional
+        the directory where the raw data are stored. For most of the
+        time this can be left as None as the raw directory is
+        inferred from the projection files, but it is useful when
+        the original raw directory is invalid. Default to None.
+    sort_by_angle : bool, optional
+        whether to sort the projections by their rotation angles.
+        Default to False.
+    pad_to_max : bool, optional
+        whether to pad the individual projection if it is not at the
+        maximum size of the stack. Default to True. If it is False
+        and there is inconsistent size, RuntimeError is raised.
+    compress : bool, optional
+        whether to apply compression (Blosc) to the NXtomo file.
+        Default to False.
+    kwargs : dict, optional
+        options for ptycho-tomography
 
-    proj_dir : Path
-
-    nxtomo_dir : Path
-
-    include_scan : list
-
-    include_proj : list
-
-    include_angle : list
-
-    facility : str
-
-    raw_dir : Path
     """
     # initiate instance for experiment
-    tomo_expt = select_tomo_expt(experiment_type, facility, proj_dir,
-                                 nxtomo_dir, include_scan, include_proj,
-                                 include_angle, raw_dir,
-                                 sort_by_angle=sort_by_angle,
-                                 pad_to_max=pad_to_max, compress=compress,
-                                 **kwargs)
+    tomo_expt = select_tomo_expt(
+        experiment_type,
+        facility,
+        proj_dir,
+        nxtomo_dir,
+        include_scan,
+        include_proj,
+        include_angle,
+        raw_dir,
+        sort_by_angle=sort_by_angle,
+        pad_to_max=pad_to_max,
+        compress=compress,
+        **kwargs,
+    )
 
     logger.info("Start finding projections...")
     st = time.perf_counter()
@@ -51,8 +100,9 @@ def tomojoin(experiment_type, facility=None, proj_dir=None, nxtomo_dir=None,
     elapse = time.perf_counter() - st
     logger.info(f"The duration of finding projections: {elapse:.2f} s")
     logger.info(f"{tomo_expt.num_projections} projections have been found.")
-    logger.info(f"The raw data directory is "
-                f"{tomo_expt.projections[0].raw_dir}")
+    logger.info(
+        f"The raw data directory is " f"{tomo_expt.projections[0].raw_dir}"
+    )
 
     # associate projections with projection angles
     tomo_expt.extract_projections_details()
