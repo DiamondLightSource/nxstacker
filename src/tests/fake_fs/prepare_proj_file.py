4fb437c8baada9e92f8c305e6f33951acfe6a062
Timothy Poon <timothy.poon@diamond.ac.uk>
2024-05-24 11:21:33 +0100

    Add class to prepare minimal PtyREX file for testing

--- src/tests/fake_fs/prepare_proj_file.py
+++ src/tests/fake_fs/prepare_proj_file.py
@@ -1,3 +1,5 @@
+import random
+from datetime import datetime
 from pathlib import Path
 
 import h5py
@@ -75,6 +77,92 @@ class PreparePtyPyFile:
                 f[f"/content/obj/{self.storage}/_psize"] = (
                     self.y_px_size,
                     self.x_px_size,
+                )
+
+            self.proj_files.append(fp)
+
+
+class PreparePtyREXFile:
+    """Prepare minimal PtyREX file for testing."""
+
+    def __init__(
+        self,
+        dest,
+        scan_num,
+        proj_num,
+        ob_shape,
+        x_px_size,
+        y_px_size,
+        detector_distance,
+    ):
+        """Initialise the instance for preparation.
+
+        Parameters
+        ----------
+        dest : str or pathlib.Path
+            the directory to save the files
+        scan_num : int
+            the scan number from which they are reconstructed
+        proj_num : list
+            the projection number(s) from which they are reconstructed
+        raw_files : list
+            the raw file(s) from which they are reconstructed
+        ob_shape : iterable
+            the 2D shape of the object
+        x_px_size, y_px_size : float
+            the x and y real-space pixel size
+        detector_distance : float
+            the sample-to-detector distance
+
+        """
+        self.scan_num = [scan_num] * len(proj_num)
+        self.proj_num = proj_num
+        self.ob_sh = (2, 2, 2, 2, 2, *ob_shape)
+        self.x_px_size = x_px_size
+        self.y_px_size = y_px_size
+        self.pr_sh = (2, 2, 2, 2, 2, 128, 128)
+        self.detector_distance = detector_distance
+
+        # create directory to store the files
+        self.proj_dir = Path(dest) / "proj_dir"
+        self.proj_dir.mkdir(parents=True)
+
+        self.proj_files = []
+        self.prefix = ""
+
+    def write_dummy_proj(self):
+        """Save a minimal dummy PtyREX reconstruction file."""
+        rng = np.random.default_rng()
+
+        for sn, pn in zip(self.scan_num, self.proj_num, strict=False):
+            ext = random.choice(("hdf", "hdf5", "h5"))  # noqa: S311
+            timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")  # noqa: DTZ005
+            fp = self.proj_dir / f"{self.prefix}_{sn}_{pn}_{timestamp}.{ext}"
+
+            with h5py.File(fp, "w") as f:
+                f["/entry_1/experiment_1/data/data_ID"] = str(pn)
+
+                f["/entry_1/process_1/common_1/save_dir"] = str(self.proj_dir)
+
+                f["/entry_1/process_1/common_1/dx"] = np.array(
+                    [[self.y_px_size, self.x_px_size]]
+                )
+
+                f["/entry_1/process_1/output_1/object_modulus"] = rng.random(
+                    self.ob_sh, dtype=np.float32
+                )
+                f["/entry_1/process_1/output_1/object_phase"] = rng.random(
+                    self.ob_sh, dtype=np.float32
+                )
+                f["/entry_1/process_1/output_1/probe_modulus"] = rng.random(
+                    self.pr_sh, dtype=np.float32
+                )
+                f["/entry_1/process_1/output_1/probe_phase"] = rng.random(
+                    self.pr_sh, dtype=np.float32
+                )
+
+                f["/entry_1/experiment_1/detector/distance"] = (
+                    self.detector_distance
                 )
 
             self.proj_files.append(fp)
